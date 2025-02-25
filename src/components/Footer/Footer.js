eb942937244bda589d9a04df81e6a91faa9e0595
datmt <3121410143@sv.sgu.edu.vn>
2025-02-26 00:56:41 +0700

    portfolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/datmaithanh'
+      className='link footer__link'
+    >
+      Created By Thanh Dat
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,75 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://github.com/datmaithanh',
+  title: 'DEV.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Thanh Dat',
+  role: 'Full Stack Developer',
+  description: 'I am a website developer, specializing in Spring and Spring Boot for backend development, and React for frontend development. I have the ability to build and optimize web applications from the database to the user interface, ensuring high performance and the best experience for users. With logical thinking, problem-solving skills, and a learning spirit, I always seek to apply new technologies to improve product quality. I love working with the Java ecosystem, Restful API, Microservices, and developing quick-reacting applications with React. Please cooperate with me to create quality products!',
+  resume: 'https://github.com/datmaithanh',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com/datmaithanh',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'Spring Boot',
+  'Spring framework',
+  'Java',
+  'React',
+  'JavaScript',
+  'HTML',
+  'CSS',
+  'Redux',
+  'Hooks',
+  'SASS',
+  'Git',
+  'GitHub',
+  'CI/CD',
+  'Docker',
+  'Microservices'
+
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'datmt07@mail.com',
+  phone: '0867874153',
+}
+
+export { header, about, projects, skills, contact }
